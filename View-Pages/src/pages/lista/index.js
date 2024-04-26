dea7b0c9a124bda40ee0191bea14775a4cc2e80a
KreativeSystem <kreativesystemcharlie@gmail.com>
2024-04-26 16:47:42 -0300

    update files changes - 26/04
    Hoje adicionamos ao front-end o método GET que pegas as informações do banco e mostra na tela listar. Tem um código não terminado a partir da linha 198 que são os modais para o visualizar e editar.

--- View-Pages/src/pages/lista/index.js
+++ View-Pages/src/pages/lista/index.js
@@ -1,12 +1,52 @@
-import React from "react";
-import Login from "../login";
+import React, { useState, useEffect } from "react";
+import api from "../../config/configApi";
 
 export const Lista = () => {
-    return(
- <div>
 
-    {/* HEADER */}
-    {/* <header>
+    const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        fetchUsers()
+    }, []);
+
+    const fetchUsers = async () => {
+        try {
+            const response = await api.get('/users');
+            setUsers(response.data.users || []); // Certifica-se de que users seja inicializado como uma matriz vazia se não houver nenhum usuário retornado
+            setLoading(false);
+        } catch (error) {
+            console.error("Erro ao carregar usuários:", error);
+        }
+    };
+
+    const modal = (user) => {
+
+        <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="staticBackdropLabel">Informações<br />{user.name}</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Understood</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    }
+
+    return (
+        <div>
+
+            {/* HEADER */}
+            {/* <header>
         <nav class="navbar navbar-expand-lg bg-body-tertiary header" data-bs-theme="dark">
                 <div class="container-fluid ">
                     <img src="/img/bootstrap.png" alt=""/>
@@ -57,7 +97,7 @@ export const Lista = () => {
         </nav>
     </header> */}
 
-<header>
+            <header>
                 <nav className="navbar navbar-expand-lg cor-header ">
                     <div className="container-fluid">
 
@@ -117,90 +157,90 @@ export const Lista = () => {
 
             </header>
 
-    {/* FIM DO HEADER */}
-<div class="table-responsive">
-      <div class="tabela">
-            <table className="table table-sm tabela">
-                <thead>
-                    <div className="titulo ">
-                            Lista
-                            <button type="button" className="botao">Cadastrar</button>
-                    </div>
-                        
+            {/* FIM DO HEADER */}
+            <div class="table-responsive">
+                <div class="tabela">
+                    <table className="table table-sm tabela">
+                        <thead>
+                            <div className="titulo ">
+                                Lista
+                                <button type="button" className="botao">Cadastrar</button>
+                            </div>
 
 
-                        <tr>
-                        <th scope="col">ID</th>
-                        <th scope="col">Nome</th>
-                        <th scope="col">E-mail</th>
-                        <th scope="col">Coluna 4</th>
-                        <th scope="col">Coluna 5</th>
-                        <th scope="col">Coluna 6</th>
-                        <th scope="col">Ações</th>
-                        </tr>
-                </thead>
-            <tbody>
-                    <tr>
-                        <th scope="row">1</th>
-                        <td>Estella</td>
-                        <td>estlledias@gmail.com</td>
-                        <td>coluna 4</td>
-                        <td>coluna 5</td>
-                        <td>coluna 6</td>
-                        <td><button type="button" class="btn btn-primary">Ações</button></td>
-                        
-                    </tr>
 
-                    <tr>
-                        <th scope="row">2</th>
-                        <td>Estella</td>
-                        <td>estlledias@gmail.com</td>
-                        <td>coluna 4</td>
-                        <td>coluna 5</td>
-                        <td>coluna 6</td>
-                        <td><button type="button" class="btn btn-primary">Ações</button></td>
-                        
-                       
-                    </tr>
+                            <tr>
+                                <th scope="col">ID</th>
+                                <th scope="col">Nome</th>
+                                <th scope="col">E-mail</th>
+                                <th scope="col">Telefone</th>
+                                <th scope="col">Ações</th>
+                            </tr>
+                        </thead>
+                        <tbody>
 
-                    <tr>
-                        <th scope="row">3</th>
-                        <td>Estella</td>
-                        <td>estlledias@gmail.com</td>
-                        <td>coluna 4</td>
-                        <td>coluna 5</td>
-                        <td>coluna 6</td>
-                        <td><button type="button" class="btn btn-primary">Ações</button></td>
-                    </tr>
-                
+                            {users.map(user => (
+                                <tr key={user.id}>
+                                    <td>{user.id}</td>
+                                    <td>{user.name}</td>
+                                    <td>{user.email}</td>
+                                    <td>{user.phone}</td>
+                                    <td>
+                                        {/* Adicione os botões de ação aqui */}
+                                        <button className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye-fill" viewBox="0 0 16 16">
+                                                <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0" />
+                                                <path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8m8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7" />
+                                            </svg>
+                                        </button>
 
-            </tbody>
-            </table>
-        </div>
+                                        <button className="btn btn-success" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
+                                                <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.5.5 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11z" />
+                                            </svg>
+                                        </button>
+
+                                        <button className="btn btn-danger" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+                                                <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5M8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5m3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0" />
+                                            </svg>
+                                        </button>
+
+                                    </td>
+                                </tr>
+
+                                /* 
+                                    fazer o modal para o editar e visualizar
+                                */
+                            ))}
+
+                        </tbody>
+                    </table>
+                </div>
 
 
-        <nav aria-label="Page navigation example">
+                <nav aria-label="Page navigation example">
 
-            <ul class="pagination justify-content-center"> 
-              <li class="page-item">
-                <a class="page-link" href="#" aria-label="Previous">
-                  <span aria-hidden="true">&laquo;</span>
-                </a>
-              </li>
-              <li class="page-item"><a class="page-link" href="#">1</a></li>
-              <li class="page-item"><a class="page-link" href="#">2</a></li>
-              <li class="page-item"><a class="page-link" href="#">3</a></li>
-              <li class="page-item">
-                <a class="page-link" href="#" aria-label="Next">
-                  <span aria-hidden="true">&raquo;</span>
-                </a>
-              </li>
-            </ul>
-        
-          </nav>
-    </div>
+                    <ul class="pagination justify-content-center">
+                        <li class="page-item">
+                            <a class="page-link" href="#" aria-label="Previous">
+                                <span aria-hidden="true">&laquo;</span>
+                            </a>
+                        </li>
+                        <li class="page-item"><a class="page-link" href="#">1</a></li>
+                        <li class="page-item"><a class="page-link" href="#">2</a></li>
+                        <li class="page-item"><a class="page-link" href="#">3</a></li>
+                        <li class="page-item">
+                            <a class="page-link" href="#" aria-label="Next">
+                                <span aria-hidden="true">&raquo;</span>
+                            </a>
+                        </li>
+                    </ul>
 
-</div>
+                </nav>
+            </div>
+
+        </div >
     )
 }
 
